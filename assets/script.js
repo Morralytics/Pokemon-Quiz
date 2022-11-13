f715ca30df4f3f82a898cee9231933663eebe156
Nick <morrinic@oregonstate.edu>
2022-11-13 00:48:08 -0800

    Continuing with commits throughout project. Was able to return user selected items and compare to the correct answer listed.

--- assets/script.js
+++ assets/script.js
@@ -3,69 +3,115 @@ var introductionBox = document.querySelector(".introduction-page");
 var scoreTrack = document.querySelector(".header-score");
 var timerTrack = document.querySelector(".header-time");
 
-// var option1Check = document.getElementById("option1").innerHTML;
-// var option2Check = document.getElementById("option2").innerHTML;
-// var option3Check = document.getElementById("option3").innerHTML;
-// var option4Check = document.getElementById("option4").innerHTML;
-// var option5Check = document.getElementById("option4").innerHTML;
-// var timeRemaining = document.getElementById("interval").innerHTML;
+var nextButton = document.querySelector(".next-button");
 
+var timeRemaining = document.getElementById("interval").innerHTML;
 
+var questionCount = 0;
 
+// Instead creating list with all values to pull from
+var questions = [
+    {
+        num: 1,
+        question: "Which is one of the starter Pokemon you can chose as your partner from Generation 1?",
+        answer: "Charmander",
+        options: ["Butterfree",
+            "Pikachu",
+            "Charmander",
+            "Eevee"
+        ]
+    }, {
+        num: 2,
+        question: "Which Pokemon type is most effective against a Flying Pokemon?",
+        answer: "Electric",
+        options: ["Water",
+            "Dragon",
+            "Rock",
+            "Electric"
+        ]
+    }, {
+        num: 3,
+        question: "Who is the lead protagonist in the Pokemon Anime?",
+        answer: "Ash Ketchum",
+        options: ["Brock",
+            "Ash Ketchum",
+            "Misty",
+            "Pikachu"
+        ]
+    }, {
+        num: 4,
+        question: "What is the name of the first ever Pokemon Game released in 1996?",
+        answer: "Pokemon Red & Blue",
+        options: ["Pokemon Red & Blue",
+            "Pokemon Yellow",
+            "Pokemon Emerald",
+            "Pokemon FireRed & LeafGreen"
+        ]
+    }, {
+        num: 5,
+        question: "Finish the lyrics: 'I wanna be the very best, like no one ever was. To catch them is my real test...'?",
+        answer: "To train them is my cause!",
+        options: ["The power that's inside!",
+            "Gotta catch 'em all!",
+            "To train them is my cause!",
+            "I will battle every day!"
+        ]
+    }
+]
 
-var createQ1 = function() {
-    //need to doc.create element and append child for each question
-    //ugh.
-    //Luckily it is already coded in the html for structure
-    //Do this in the morning
+// Displays each question as it cycles through
+var displayQuestions = function (index) {
+    // Pulls the information from the HTML
+    var qText = document.querySelector(".question");
+    var qNum = document.querySelector(".q-num");
+    var nextBox = document.querySelector(".next-button");
+    var optionList = document.querySelector(".options");
+
+    // Setting the attribute to match the styling I am looking for as it appears
+    qNum.setAttribute("style", "font-weight:bold; font-size:32px");
+    qText.setAttribute("style", "font-weight:bold; font-size:32px");
+    
+    // Creating the values I want added into easy to repeat variables
+    var nextBoxInit = "<button class='next'> Next Question </button>";
+    var qInit = questions[index].question;
+    var numInit = "Question " + questions[index].num;
+    var optionInit = "<div class='optionItm'>" + questions[index].options[0] + "</div>" +
+    "<div class='optionItm'>" + questions[index].options[1] + "</div>" +
+    "<div class='optionItm'>" + questions[index].options[2] + "</div>" +
+    "<div class='optionItm'>" + questions[index].options[3] + "</div>";
+    
+    
+    // Setting the text content and inner HTML values to what was previously set
+    // Text content is used for the use of strict HTML written code
+    // Inner HTML is used for more complex structures like inclusion of divs
+    qText.textContent = qInit;
+    qNum.textContent = numInit;
+    optionList.innerHTML = optionInit;
+    nextBox.innerHTML = nextBoxInit;
+
+    // Cycles through the option list while setting onclick effects for grabbing user clicked information
+    // When an option is clicked, it will run a function setting the users answer
+    // Onclick is used for this particular for loop
+    var option = optionList.querySelectorAll(".optionItm");
+    for (i = 0; i < option.length; i++) {
+       option[i].setAttribute("onclick", "userOption(this)"); 
+    }
 }
 
+var userOption = function(answer) {
+    var userAnswer = answer.textContent;
+    var correctAnswer = questions[questionCount].answer;
+    
+    if (userAnswer == correctAnswer) {
+        console.log("correct");
+    } else {
+        console.log("incorrect");
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// var information = function() {
-//     var numOptions = [];
-
-//     if (option1Check) {
-//         numOptions.push(option1Check);
-//     } 
-//     console.log(numOptions);
-// }
-
-// information();
-
-
-
-
-
-
-
-// var documentEl = function() {
-
-//     var optionArr = [];
-//     var index = 0;
-
-//     var divs = document.querySelectorAll(".question-container div");
-
-//     if (index < divs.length) {
-//         index++;
-//         optionArr.push(divs)
-//     }
-//     console.log(optionArr);
-// }
-
-startQuizButton.addEventListener("click", function() {
-
-    var timerInterval = setInterval(function() {
+// Event triggered when start quiz button is clicked
+startQuizButton.addEventListener("click", function () {
+    var timerInterval = setInterval(function () {
         if (timeRemaining > 0) {
             timeRemaining--;
             timerTrack.innerHTML = "Time: " + timeRemaining;
@@ -74,8 +120,18 @@ startQuizButton.addEventListener("click", function() {
             timerTrack.innerHTML = "Times Up!";
         }
     }, 1000);
-
+    
     introductionBox.setAttribute("style", "display:none");
+    displayQuestions(questionCount);
 });
 
-documentEl();
+// Next button being clicked
+nextButton.addEventListener("click", function() {
+    if (questionCount < questions.length -1) {
+        questionCount++;
+        displayQuestions(questionCount);
+    } else {
+        console.log("Finshed quiz");
+    }
+});
+
